d8f7b09fe28dac3a429d86415b3dd25de9dd12dd
Edvinas Sadlevicius <hummanlitto@gmail.com>
2021-03-18 14:09:29 +0200

    Start

---
+++ index.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html>
+    <head>
+    </head>
+    <body>
+        <header>
+            <div class="logo-container">
+                <img src="" alt="logo">
+            </div>
+        </header>
+            <section>
+                <div>Neleiskite, kad prarasti dantys taptu nuosprendžiu !</div>
+                <div>Kodėl netenkame dantų ?</div>
+                <ul class="example-modals">
+                    <?php for($i = 0;$i < 3;$i++)
+                    {?>
+                    <li class="example-modal-<?php echo $i?>">
+                        <img src=""/>
+                        <p></p>
+                    </li>
+                    <?php }?>
+                </ul>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Optio quo excepturi quaerat necessitatibus ducimus saepe libero velit unde, 
+                nisi aspernatur magni voluptates sit repellat a vitae itaque, ratione delectus cupiditate.lore</p>
+            </section>
+            <section>
+                <div>03<div class="line"></div></div>
+                <div>Gydytojai pataria</div>
+                <div>
+                        <img/>
+                        <div>
+                            <div>Name</div>
+                            <div>Position</div>
+                            <hr>
+                            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Qui architecto tenetur sint cupiditate maxime voluptatum totam in commodi quisquam deserunt, 
+                            eum, autem obcaecati accusamus, distinctio explicabo? Suscipit cumque quia obcaecati?</p>
+                        </div>
+                </div>
+            </section>
+            <section>
+                <p>Pradėti yra praprasta<br>
+                <strong>ženkite pirmą žingsnį</strong></p>
+                <p>Registracija nemokamai konsultacijai</p>
+                <p>Liko <strong>9</strong> nemokamos registracijos</p>
+                <form>
+                        <div>
+                            <input type="text" placeholder="Vardas, pavardė"/>
+                            <input type="text" placeholder="Telefonas"/>
+                        </div>
+                        <button>Registruotis</button>
+                </form>
+            </section>
+    </body>
+</html>
